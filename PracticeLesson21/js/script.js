13e9077bdd260dbbda387ecc90f7dd2561fac684
alexei-shevchenko <shevchenkoalexv22@gmail.com>
2022-06-13 16:08:53 +0300

    Lesson 21

---
+++ PracticeLesson21/js/script.js
@@ -0,0 +1,77 @@
+'use strict';
+
+// Логические операторы, урок №21 (д)
+
+// const hamburger = true;
+// const fries = true;
+
+// if (hamburger && fries) {
+//     console.log('Я сыт!');
+// }
+
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 1;
+
+// console.log(hamburger === 3 && cola && fries);
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && 'asdfwere');
+
+// if (hamburger === 3 && cola === 1 && fries) {
+//     console.log('Все сыты!');
+// } else {
+//     console.log("Мы уходим");
+// }
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+//     console.log('Все довольны!');
+// } else {
+//     console.log("Мы уходим");
+// }
+
+// console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
+
+let johnReport, alexReport, samReport, mariaReport = 'done';
+
+console.log(johnReport || alexReport || samReport || mariaReport);
+
+console.log(!0);
+
+// Домашнее задание
+
+console.log(NaN || 2 || undefined);
+
+console.log(NaN && 2 && undefined);
+
+console.log(1 && 2 && 3);
+
+console.log(!1 && 2 || !3);
+
+console.log(25 || null && !3);
+
+console.log(NaN || null || !3 || undefined || 5);
+
+console.log(NaN || null && !3 && undefined || 5);
+
+console.log(5 === 5 && 3 > 1 || 5);
+
+console.log(3 > 1);
+
+// Выполнится ли условие?
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger || cola || fries === 3 || nuggets) {
+   console.log('Done!');
+}
