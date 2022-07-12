e8b59c03b075934e9b67bcc39448e51c3cb8e0c1
alexei-shevchenko <shevchenkoalexv22@gmail.com>
2022-07-12 15:29:11 +0300

    practice ex 10-12

--- PracticeExercise10-11-12/js/script.js
+++ PracticeExercise10-11-12/js/script.js
@@ -18,10 +18,10 @@ const personalPlanPeter = {
     showAgeAndLangs: function(plan) {
         const {age} = plan;
         const {languages} = plan.skills;
-        let str = `Мне ${age}и я владею языками: `;
+        let str = `Мне ${age} и я владею языками: `;
 
         languages.forEach(function(lang) {
-            str += `${lang.toUpperCase()}`;
+            str += `${lang.toUpperCase()} `;
         });
 
         return str;
@@ -51,3 +51,107 @@ function showProgrammingLangs(plan) {
     return str;
 }
 console.log(showProgrammingLangs(personalPlanPeter));
+
+
+// Упражнение по написанию кода №11 (работа с массивами)
+
+// Первое задание
+
+const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+
+function showFamily(arr) {
+    let str = '';
+
+    arr.length === 0 ? str = 'Семья пуста' : str = 'Семья состоит из: ';
+
+    arr.forEach(member => {
+        str += `${member} `;
+    });
+    
+    return str;
+}
+
+console.log(showFamily(family));
+
+// Второе задание
+// моё решение
+const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+function standardizeStrings(arr) {
+    for (let value of arr) {
+        console.log(value.toLowerCase());
+    }
+}
+
+standardizeStrings(favoriteCities);
+
+// решение по курсу
+
+function standardizeStrings(arr) {
+    arr.forEach(city => {
+        console.log(city.toLowerCase());
+    });
+}
+
+// Упражнение по написанию кода №12 (работа с массивами ч.2)
+
+// Третье задание
+
+// Моё решение
+
+const someString = 'This is some strange string';
+
+function reverse(str) {
+    if (typeof(str) !== 'string') {
+        return 'Ошибка!';
+    }
+
+    let strReverse = str.split('');
+    strReverse.reverse();
+    
+    let newStr = strReverse.join('');
+    return newStr;
+
+}
+
+console.log(reverse(someString));
+
+// Решение по курсу
+
+function reverse(str) {
+    if (typeof(str) !== 'string') {
+        return "Ошибка!";
+    }
+    // Самый оптимальный вариант решения
+    return str.split('').reverse().join('');
+
+    // Решение при помощи цикла
+    // let newStr = '';
+    // for (let i = str.length - 1; i >= 0; i--) {
+    //     newStr += str[i];
+    // }
+    // return newStr;
+}
+
+// Четвёртое задание
+
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+const b = [];
+
+function availableCurr(arr, missingCurr) {
+    let str = '';
+
+    arr.length === 0 ? str = 'Нет доступных валют' : str = 'Доступные валюты:\n';
+
+    arr.forEach(function(curr, i) { // Не совсем понимаю для чего 'i', работает и без неё
+        if (curr !== missingCurr) {
+            str += `${curr}\n`;
+        }
+    });
+
+    return str;
+}
+
+console.log(availableCurr([...baseCurrencies, ...additionalCurrencies], 'CNY'));
+
