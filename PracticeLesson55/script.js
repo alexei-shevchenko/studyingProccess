87eec4322c5c659b150e73077d886fbbb5b9ac40
alexei-shevchenko <shevchenkoalexv22@gmail.com>
2022-07-28 22:20:03 +0300

    lesson 55

--- PracticeLesson55/script.js
+++ PracticeLesson55/script.js
@@ -1,2 +1,25 @@
 'use strict';
 
+const boxesQuery = document.querySelectorAll('.box'); // Статичные коллекции (показывает в консоли псевдомассивы, на момент вызова команды, они не знают что было после)
+const boxesGet = document.getElementsByClassName('box'); // Живые коллекции (показывает в консоли псевдомассивы, которые существуют на данный момент)
+
+boxesQuery.forEach(box => {
+    if (box.matches('.this')) console.log(box);
+});
+
+console.log(boxesQuery[0].closest('.wrapper'));
+
+// boxesQuery[0].remove();
+// boxesGet[0].remove();
+
+// for (let i = 0; i < 5; i++) {
+//     const div = document.createElement('div');
+//     div.classList.add('box');
+//     document.body.append(div);
+// }
+
+// console.log(boxesQuery);
+// console.log(boxesGet);
+// // console.log(document.body.children);
+
+// console.log(Array.from(boxesGet));
