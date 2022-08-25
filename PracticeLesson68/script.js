55de5e0f40026471d4a62399634056fe558000a1
alexei-shevchenko <shevchenkoalexv22@gmail.com>
2022-08-25 21:04:30 +0300

    Lesson 68

---
+++ PracticeLesson68/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const now = new Date('2022-08-25');
+// new Date.parse('2022-08-25');
+
+console.log(now.setHours(18));
+console.log(now);
+console.log('hello');
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
+
+// console.log(now.getTimezoneOffset());
+// console.log(now.getTime());
+
+let start = new Date();
+
+for (let i = 0; i < 100000; i++) {
+    let some = i ** 3;
+}
+
+let end = new Date();
+
+alert(`Цикл отработал за ${end - start} милисекунд`);
